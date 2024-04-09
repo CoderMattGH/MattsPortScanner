b5833a62cddbb4fd13f21f3e96974d059049c8ad
GANDHIX <mattdixon2000@hotmail.com>
2024-04-09 11:43:37 +0100

    Added compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-gcc portscanner.c ./my_includes/network_helper.c ./my_includes/packet_service.c -o portscanner
+gcc portscanner.c ./my_includes/network_helper.c ./my_includes/packet_service.c ./my_includes/arp_service.c -o portscanner
 
